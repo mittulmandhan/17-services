d67ae40579ae1b8c39d9733f5696a1b1bd8a45b4
mittulmandhan <conanmittul@gmail.com>
2019-07-31 04:28:49 +0530

    services completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+  // App module bootstraps the AppComponent
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-employee-list',
-  templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  selector: 'app-employee-detail',
+  templateUrl: './employee-detail.component.html',
+  styleUrls: ['./employee-detail.component.css']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeDetailComponent implements OnInit {
 
-  // this is a hard coded array of employees
-  // each employee has id, name and age
-
+  // copy of employees object is created in order to access the object
   public employees = [
     { id: 1, name: 'sagar', age: 30 },
     { id: 2, name: 'rahul', age: 20 },
     { id: 3, name: 'simran', age: 24 },
     { id: 4, name: 'yogesh', age: 75 }
   ];
-
   constructor() { }
 
   ngOnInit() {
